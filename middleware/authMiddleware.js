d9fc1852f85d813bc81d73e6ced911980413fa17
Kysohee18 <hanashahky@gmail.com>
2026-07-27 10:48:44 +0700

    add: implement authentication middleware for token validation

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+    try {
+        const authHeader = req.header.authorization;
+        if (!authHeader){
+            return res.status(401).json({
+                message:"authorization token tidak ditemukan"
+            });
+        }
+        const token = authHeader.split(" ")[1];
+        if (!token) {
+            return res.status(401).json({
+                message: "format token tidak valid"
+            });
+        }
+        const decoded = jwt.verify{token, process.env. JWT_SECRET};
+        req.user = decoded
+        next();
+
+    } catch (error) {
+        return res.status(401).json({
+            message: "token tidak vaild atau telah kadaluarsa"
+        });
+    }
+};
